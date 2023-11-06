bcb0618f58c77fc811b177dc4d1fca5f520fa0f3
facunare <arechaga.facundoet36@gmail.com>
2023-11-06 12:21:11 -0300

    sin intentos

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import socket
 import pickle
+
+intentos = 5
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_client.connect(("localhost", 8001))
 print("Esperando a que el jugador 1 elija un numero")
@@ -11,6 +13,13 @@ while True:
     socket_client.send(mensaje.encode())
     rta = socket_client.recv(1024).decode()
     print("Servidor: "+ str(rta))
-    if str(rta) == "Acertaste":
+    if str(rta) == f"\033[94mAcertaste\033[0m":
         break
+    else:
+        intentos-=1
+        if intentos == 0:
+            sinIntentos = f"\033[91mTe quedaste sin intentos!!\033[0m" 
+            print(sinIntentos)
+            break
+
 socket_client.close()
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ socket_sv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_sv.bind(("localhost", 8001))
 socket_sv.listen(1)
 socket_client, client_addr = socket_sv.accept()
+intentos = 5
 while True:
     numero = int(input("Elija un numero del 1 al 100: "))
     if numero == "" or numero <= 0 or numero >100:
@@ -17,28 +18,34 @@ while True:
 
 while True:
     rta = socket_client.recv(1024).decode()
-    print("El jugador elijio el numero: "+ str(rta))
+    print("El jugador elijio el numero: "+ f"\033[94m{str(rta)}\033[0m" )
     resta = numero - int(rta)
     if resta<=0:
         resta = resta*-1
-
-    if int(rta) > numero and (resta>= 1 and resta <=20):
-        mensaje = "El numero es menor y estas muy cerca"
-    elif int(rta) > numero and (resta >20 and resta <=60):
-        mensaje = "El numero es menor"
-    elif int(rta) > numero and (resta >60 and resta <=99):
-        mensaje = "El numero es menor y estas lejos"
-    elif int(rta) < numero and (resta>= 1 and resta <=20):
-        mensaje = "El numero es mayor y estas muy cerca"
-    elif int(rta) < numero and (resta >20 and resta <=60):
-        mensaje = "El numero es mayor"
-    elif int(rta) < numero and (resta >60 and resta <=99):
-        mensaje = "El numero es mayor y estas lejos"
-    elif int(rta) == numero:
-        mensaje = f"Acertaste"
+    if int(rta) == numero:
+        mensaje = f"\033[94mAcertaste\033[0m" 
         socket_client.send(mensaje.encode())
         break
     else:
-        print("Ninguno")
-
+        print("hola")
+        intentos-=1
+        if intentos == 0:
+            sinIntentos = f"\033[91mEl client se quedo sin intentos!!\033[0m" 
+            print(sinIntentos)
+            break
+        
+    if int(rta) > numero and (resta>= 1 and resta <=20):
+        mensaje = f"\033[91mEl numero es menor y estas muy cerca\033[0m" 
+    elif int(rta) > numero and (resta >20 and resta <=60):
+        mensaje = f"\033[91mEl numero es menor\033[0m" 
+    elif int(rta) > numero and (resta >60 and resta <=99):
+        mensaje = f"\033[91mEl numero es menor y estas lejos\033[0m" 
+    elif int(rta) < numero and (resta>= 1 and resta <=20):
+        mensaje = f"\033[91mEl numero es mayor y estas muy cerca\033[0m" 
+    elif int(rta) < numero and (resta >20 and resta <=60):
+        mensaje = f"\033[91mEl numero es mayor\033[0m" 
+    elif int(rta) < numero and (resta >60 and resta <=99):
+        mensaje = f"\033[91mEl numero es mayor y estas lejos\033[0m" 
+    
+    
     socket_client.send(mensaje.encode())
